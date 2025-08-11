b5e27d01e3099af1f4fb7e4b91b620a9aab358b2
Sergey Gubarev <s.gubarev1991@gmail.com>
2025-08-11 19:03:43 +0300

    Changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+        int balance = 257; // Начальный баланс
+        int payment = 1500; // Сумма платежа
+        int bonus = 0;
+        if (payment > 1000) {
+            bonus = payment / 100;
+        }
+        int newBalance = balance + payment + bonus;
+        System.out.println("Количество бонусов = " + bonus);
+        System.out.println("Итоговая сумма = " + newBalance);
+    }
 }
